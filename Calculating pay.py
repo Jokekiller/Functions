01b0c2d9c555ca5d4c35e2c062f7d195c778473f
Harry Robinson <41194@longroad.ac.uk>
2014-11-24 12:18:23 +0000

    calculating pay

--- Calculating pay.py
+++ Calculating pay.py
@@ -3,22 +3,35 @@
 #Program calculating pay
 
 def calculateBasicPay (hours,pay):
-    basicPay = hours*pay
+    basicPay = hours * pay
     return basicPay
 def calculateOvertimePay (overTimeHours,overTimePay):
-    overTimeHours = hours - 40
-    overTimePay = overTimeHours * (1.5 * pay)
-    totalOvertimePay = overtimeHours * overtimePay
-    basicPay = 40 * pay
+    overTimeHours = overTimeHours - 40
+    overTimePay = overTimeHours * (1.5 * overTimePay)
+    totalOvertimePay = overTimeHours * overTimePay
+    basicPay = 40 * overTimePay
     totalPay = basciPay + totalOvertimePay
     return overTimePay
 def workDetails ():
     hours = int(input("Enter the hours worked this week: "))
-    pay = int(input("Enter your pay: "))
+    pay = float(input("Enter your pay: "))
+    return hours, pay
+def calculateTotalPay(hours,pay):
+    if hours <= 40:
+        totalPay = calculateBasicPay(hours,pay)
+    else:
+        totalPay = calculateOvertimePay(hours,pay)
+    return totalPay
 def displayTotalPay(totalPay):
+    print(totalPay)
+def calculatePay():
+    hours, pay = workDetails()
+    totalPay = calculateTotalPay(hours, pay)
+    displayTotalPay(totalPay)
+    
+
     
               
 
 #main program
-total = calculateBasicPay(30,5)
-print(total)
+calculatePay()
