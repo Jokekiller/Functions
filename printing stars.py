1f58c8866ec0ed47a19c1c96e178be708f0adb6a
Harry <Harry@192.168.0.17>
2014-12-09 12:19:38 +0000

    Printing stars

--- printing stars.py
+++ printing stars.py
@@ -2,6 +2,8 @@
 #02-12-2014
 #Stars program
 
+row = 0
+
 def star_details():
     number_of_stars = int(input("Enter a number: "))
     return number_of_stars
@@ -14,9 +16,9 @@ def number_verification(number_of_stars):
             print("number is not odd, try again")
     return number_of_stars
 def printing_stars(number_of_stars):
-    for row in range(number_of_stars):
+    while row in range(number_of_stars):
+        print("{0:^ 2}".format(number_of_stars), "*"* number_of_stars)
         number_of_stars = number_of_stars - 2
-        print("*"* number_of_stars)
 #main program
 number_of_stars = star_details()
 printing_stars(number_of_stars)
